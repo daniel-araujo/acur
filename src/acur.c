5cb23b6584eabe4987e05223b6128fd28cc3a6f0
Daniel Araujo <contact@daniel-araujo.pt>
2017-08-30 22:52:52 +0100

    Use extern inline
    Those declarations behave exactly the same without the keywords but with
    them the intention is made clear.

--- src/acur.c
+++ src/acur.c
@@ -1,27 +1,27 @@
 #include "acur.h"
 
-size_t acur_data_index(struct acur *c, size_t index);
+extern inline size_t acur_data_index(struct acur *c, size_t index);
 
-void acur_init1(struct acur *c, size_t element_size, void *data, size_t size);
+extern inline void acur_init1(struct acur *c, size_t element_size, void *data, size_t size);
 
-void acur_init2(struct acur *c, size_t element_size, void *start_data, void *end_data);
+extern inline void acur_init2(struct acur *c, size_t element_size, void *start_data, void *end_data);
 
-void acur_init3(struct acur *c, size_t element_size, void *data, size_t start_index, size_t end_index);
+extern inline void acur_init3(struct acur *c, size_t element_size, void *data, size_t start_index, size_t end_index);
 
-int acur_finished(struct acur *c);
+extern inline int acur_finished(struct acur *c);
 
-void acur_rewind(struct acur *c);
+extern inline void acur_rewind(struct acur *c);
 
-void *acur_data(struct acur *c);
+extern inline void *acur_data(struct acur *c);
 
-size_t acur_index(struct acur *c);
+extern inline size_t acur_index(struct acur *c);
 
-void *acur_element(struct acur *c);
+extern inline void *acur_element(struct acur *c);
 
-size_t acur_size(struct acur *c);
+extern inline size_t acur_size(struct acur *c);
 
-void *acur_begin(struct acur *c);
+extern inline void *acur_begin(struct acur *c);
 
-void *acur_end(struct acur *c);
+extern inline void *acur_end(struct acur *c);
 
-void acur_next(struct acur *c);
+extern inline void acur_next(struct acur *c);
